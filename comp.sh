b39ffdfb813d178a0bd8588207c16956172e0c43
chandra-79 <40532231+chandra-79@users.noreply.github.com>
2020-10-15 10:55:06 +0800

    Add files via upload

---
+++ comp.sh
@@ -0,0 +1,8 @@
+javac -d build/classes src/file/exp/FileExplorer.java
+sleep 3
+cd build/classes
+java file.exp.FileExplorer /Users/chandra/temp/FileExplorer/TestFolder
+sleep 4
+sleep 4
+sleep 4
+cd ../..
