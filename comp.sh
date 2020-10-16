9413c28d8bb7e03623c96d55bcd37f2832b6623d
Wong Keng <kaira428@gmail.com>
2020-10-16 17:03:15 +0800

    Added new Word file
    added Bank Account Analysis-WongKeng

--- comp.sh
+++
@@ -1,8 +0,0 @@
-javac -d build/classes src/file/exp/FileExplorer.java
-sleep 3
-cd build/classes
-java file.exp.FileExplorer /Users/chandra/temp/FileExplorer/TestFolder
-sleep 4
-sleep 4
-sleep 4
-cd ../..
